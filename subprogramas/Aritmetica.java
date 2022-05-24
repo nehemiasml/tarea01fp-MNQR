e443258d832c226e2749ea42b2ba3826af4f5937
nehemiasml <maikol.nehemiasml@gmail.com>
2022-05-24 15:17:52 +0000

    Aritmetica

---
+++ subprogramas/Aritmetica.java
@@ -0,0 +1,23 @@
+package subprogramas
+
+/**
+ * aritmetica
+ */
+public class Aritmetica {
+    int va, vb;
+    Aritmatica(){
+
+    }
+
+    Aritmetica(int a, int b) {
+        this.va=va;
+        this.vb=vb;
+    }
+    public int sumar(int a, int b) {
+        return a+b;  
+    }
+    public int multiplicar() {
+        return(va*vb);
+        
+    }
+}
